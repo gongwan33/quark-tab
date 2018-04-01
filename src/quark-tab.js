4a040047e7cc7fa549a245fe77c5b5397d9742d3
gongwan33 <gongwan33@gmail.com>
2018-04-01 23:21:58 +0800

    add frame

--- src/quark-tab.js
+++ src/quark-tab.js
@@ -1,6 +1,9 @@
 (function($) {
-    var tabHTML = `
-        <div>tab</div>
+    var frameHTML = `
+        <div class="qt-tabs">
+        </div>
+        <div class="qt-tab-content hide">
+        </div>
         `;
     $.fn.quarkTab = function(options) {
         var settings = $.extend({
@@ -8,10 +11,29 @@
         }, options);
         
         var instance = this;
-        $(this).append(tabHTML);
 
         this.each(function(outerIndex, tab) {
-            
+            var originContent = $(tab).clone();      
+            var pages = $(tab).find('page');
+            var frame = $('<div/>').append(frameHTML);
+
+            $(tab).children().remove();
+
+            $(pages).each(function(index, page) {
+                var name = $(page).attr('name');
+                var content = $(page).html();
+                var tabHTML = `
+                    <div class="tab">` + name + `</div>
+                    `;
+                var tabContent = `
+                    <div class="qt-tab-page">` + content + `</div>
+                    `;
+
+                $(frame).find('.qt-tabs').append(tabHTML);
+                $(frame).find('.qt-tab-content').append(tabContent);
+
+                $(tab).append(frame);
+            });
         });
     }
 })(jQuery);
