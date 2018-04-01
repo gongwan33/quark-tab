5fb81586fcba6be2533e47aa0f6c493db2114e93
gongwan33 <gongwan33@gmail.com>
2018-04-02 00:06:29 +0800

    add click event

--- src/quark-tab.js
+++ src/quark-tab.js
@@ -2,7 +2,7 @@
     var frameHTML = `
         <div class="qt-tabs">
         </div>
-        <div class="qt-tab-content hide">
+        <div class="qt-tab-content">
         </div>
         `;
     $.fn.quarkTab = function(options) {
@@ -15,7 +15,7 @@
         this.each(function(outerIndex, tab) {
             var originContent = $(tab).clone();      
             var pages = $(tab).find('page');
-            var frame = $('<div/>').append(frameHTML);
+            var frame = $('<div class="qt-tabs-frame"/>').append(frameHTML);
 
             $(tab).children().remove();
 
@@ -26,13 +26,24 @@
                     <div class="tab">` + name + `</div>
                     `;
                 var tabContent = `
-                    <div class="qt-tab-page">` + content + `</div>
+                    <div class="qt-tab-page hide">` + content + `</div>
                     `;
 
                 $(frame).find('.qt-tabs').append(tabHTML);
                 $(frame).find('.qt-tab-content').append(tabContent);
 
                 $(tab).append(frame);
+            });
+
+            $(tab).find('.qt-tab-content .qt-tab-page').first().removeClass('hide');
+
+            $(tab).find('.qt-tabs .tab').click(function(ev) {
+                var clkTab = $(ev.target).closest('.tab'); 
+                var curTab = $(ev.target).closest('.qt-tabs-frame');
+                var tabIndex = $(curTab).find('.qt-tabs .tab').index($(clkTab));
+                
+                $(curTab).find('.qt-tab-content .qt-tab-page').addClass('hide');
+                $($(curTab).find('.qt-tab-content .qt-tab-page').get(tabIndex)).removeClass('hide');
             });
         });
     }
