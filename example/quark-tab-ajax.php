5420e071124cfb203c0f1eed5bda490a11f993ac
gongwan33 <gongwan33@gmail.com>
2018-04-04 11:03:50 +0800

    add lazyload page

---
+++ example/quark-tab-ajax.php
@@ -0,0 +1,11 @@
+<?php
+$ajax_page_data = array(
+    'page_html' => '
+    <h3>Page Lazy</h3>
+    <div>Content 5</div>
+    ',
+);
+
+echo json_encode($ajax_page_data);
+
+?>
--- src/quark-tab.js
+++ src/quark-tab.js
@@ -21,36 +21,70 @@
             $(tab).children().remove();
 
             $(pages).each(function(index, page) {
-                var name = $(page).attr('name');
+                var name = $(page).attr('tabname');
+                var isLazy = (typeof $(page).attr('lazy')) == 'undefined' ? false : true;
                 var content = $(page).html();
+
                 var tabHTML = `
                     <div class="tab">` + name + `</div>
                     `;
+
                 var tabContent = `
                     <div class="qt-tab-page hide">` + content + `</div>
                     `;
 
                 $(frame).find('.qt-tabs').append(tabHTML);
-                $(frame).find('.qt-tab-content').append(tabContent);
 
+                if(isLazy) {
+                    var url = $(page).attr('src'); 
+                    tabContent = `
+                        <div class="qt-tab-page hide" lazy src="` + url + `"></div>
+                        `;
+
+               } 
+
+                $(frame).find('.qt-tab-content').append(tabContent);
                 $(tab).append(frame);
+
             });
 
-            $($(tab).find('.qt-tab-content .qt-tab-page').get(settings.defaultPage)).removeClass('hide');
-            $($(tab).find('.qt-tabs .tab').get(settings.defaultPage)).css('color', settings.tabActiveColor);
             $(tab).find('.qt-tabs').css('background-color', settings.tabColor);
 
             $(tab).find('.qt-tabs .tab').click(function(ev) {
                 var clkTab = $(ev.target).closest('.tab'); 
                 var curTab = $(ev.target).closest('.qt-tabs-frame');
                 var tabIndex = $(curTab).find('.qt-tabs .tab').index($(clkTab));
+                var curPage = $(curTab).find('.qt-tab-content .qt-tab-page').get(tabIndex);
+                var isLazy = (typeof $(curPage).attr('lazy')) == 'undefined'? false : true;
                 
                 $(curTab).find('.qt-tab-content .qt-tab-page').addClass('hide');
-                $($(curTab).find('.qt-tab-content .qt-tab-page').get(tabIndex)).removeClass('hide');
+                $(curPage).removeClass('hide');
 
                 $(curTab).find('.tab').css('color', 'black');
-                $($(curTab).find('.tab').get(tabIndex)).css('color', 'red');
+                $($(curTab).find('.tab').get(tabIndex)).css('color', settings.tabActiveColor);
+
+                if(isLazy) {
+                    var url = $(curPage).attr('src');
+                    (function(curPage) {
+                        $.ajax({
+                            url: url,      
+                            method: 'POST',
+                            dataType: 'json',
+                            success: function(data) {
+                                if(data != null && data.page_html != null) {
+                                    $(curPage).append('<div>Loading...</div>');
+                                    $(curPage).children().remove();
+                                    $(curPage).append(data.page_html);
+                                } else {
+                                    $(curPage).append('No available data.');
+                                }
+                            },
+                        });
+                    })(curPage);
+                }
             });
+
+            $($(tab).find('.qt-tabs .tab').get(settings.defaultPage)).click();
         });
     }
 })(jQuery);
